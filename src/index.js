6e57e26ad9b140d14b55a791ce87b3137ac0d8bb
doug.w <doug.wright@controlgap.com>
2020-04-18 22:54:58 -0400

    change hydrate params, add some docs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 
+// not even exporting this, only really need push event...
 const LiveViewContext = React.createContext({ pushEvent: () => {} });
 
+/**
+ * usePushEvent, just returns the push event function to push an event up the live view
+ */
 export const usePushEvent = () => {
   const { pushEvent } = React.useContext(LiveViewContext);
   return pushEvent;
 };
 
+/**
+ *
+ * @param {*} props
+ */
 export const LiveViewContextProvider = ({ children, pushEvent }) => {
   return (
     <LiveViewContext.Provider value={{ pushEvent }}>
@@ -16,13 +24,26 @@ export const LiveViewContextProvider = ({ children, pushEvent }) => {
   );
 };
 
+/**
+ * Generates a hydrate function to be used in liveview hooks
+ *
+ * Object of components that can be server rendered, and also hydrated via liveview
+ * @param {object} components
+ */
 export function genHydrate(components) {
-  return function (componentName, rootId, { props, pushEvent }) {
+  /**
+   *
+   * @param {string} componentName
+   * @param {string} rootId
+   * @param {string} pushEvent
+   * @param {object} props
+   */
+  return function hydrate(componentName, rootId, pushEvent, props = {}) {
     const Comp = components[componentName];
 
     ReactDOM.hydrate(
       <LiveViewContextProvider pushEvent={pushEvent}>
-        <Comp {...props} contexts={LiveViewContext} />
+        <Comp {...props} />
       </LiveViewContextProvider>,
       document.getElementById(rootId)
     );
